e1f36b86220af3d6ebd5c9c9369c41189022cd8a
Glutinfree <TGEvans26@chaminet.org>
2026-02-26 18:57:27 -0800

    re-tuned base

--- 2026-FRC-Robot/src/main/java/frc/robot/BuildConstants.java
+++ 2026-FRC-Robot/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-FRC-Robot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 77;
-  public static final String GIT_SHA = "1c72b5fe61a2cb95ca364743086bf92741249c2c";
-  public static final String GIT_DATE = "2026-02-21 23:06:01 EST";
+  public static final int GIT_REVISION = 79;
+  public static final String GIT_SHA = "b94a4c9758d8cca9264064018d5c1cc61e5dcd9b";
+  public static final String GIT_DATE = "2026-02-26 21:05:01 EST";
   public static final String GIT_BRANCH = "DuoShooter";
-  public static final String BUILD_DATE = "2026-02-22 21:19:09 EST";
-  public static final long BUILD_UNIX_TIME = 1771813149433L;
+  public static final String BUILD_DATE = "2026-02-26 21:55:44 EST";
+  public static final long BUILD_UNIX_TIME = 1772160944001L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
